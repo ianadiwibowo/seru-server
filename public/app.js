678adb4ad3a5dd21fecfec4931afba5add28e1fc
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-09-02 22:25:49 +0700

    Fix Materialize reference

--- public/app.js
+++ public/app.js
@@ -15,14 +15,13 @@ new Vue({
     this.ws.addEventListener('message', function (e) {
       var msg = JSON.parse(e.data);
       self.chatContent += '<div class="chip">'
-        + '<img src="' + self.gravatarURL(msg.email) + '">' // Avatar
         + msg.username
         + '</div>'
         + emojione.toImage(msg.message) + '<br/>'; // Parse emojis
-
       var element = document.getElementById('chat-messages');
       element.scrollTop = element.scrollHeight; // Auto scroll to the bottom
     });
+    M.toast({ html: 'Welcome to Seru!' })
   },
   methods: {
     send: function () {
@@ -39,19 +38,16 @@ new Vue({
     },
     join: function () {
       if (!this.email) {
-        Materialize.toast('You must enter an email', 2000);
+        M.toast({ html: 'You must enter an email' });
         return
       }
       if (!this.username) {
-        Materialize.toast('You must choose a username', 2000);
+        M.toast({ html: 'You must choose a username' });
         return
       }
       this.email = $('<p>').html(this.email).text();
       this.username = $('<p>').html(this.username).text();
       this.joined = true;
     },
-    gravatarURL: function (email) {
-      return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email);
-    }
   }
 });
